b83a8f61c82b2b903f56d15e4e81465e4aca498b
Joao Higa <Joao Higa@LAPTOP-SMBBG6E9>
2025-08-09 11:51:00 -0500

    Edit: Refactorización de DTOs como Records. Antes: Clases

--- src/main/java/com/mycompany/proyectofinal/DTO/movimiento_stock_DTO.java
+++ src/main/java/com/mycompany/proyectofinal/DTO/movimiento_stock_DTO.java
@@ -2,94 +2,29 @@ package com.mycompany.proyectofinal.DTO;
 
 import java.sql.Timestamp;
 
-public class movimiento_stock_DTO {
-    private int codMov;
-    private String codProd, tipo, motivo, referencia;
-    private int cantidad;
-    private Integer codUsuario; // puede ser null
-    private Timestamp fechaCrea, fechaModif;
+public record movimiento_stock_DTO(
+        int codMov,
+        String codProd,
+        String tipo,
+        String motivo,
+        String referencia,
+        int cantidad,
+        int codUsuario,
+        Timestamp fechaCrea,
+        Timestamp fechaModif) {
 
-    public movimiento_stock_DTO(int codMov, String codProd, String tipo, String motivo, String referencia, int cantidad, Integer codUsuario, Timestamp fechaCrea, Timestamp fechaModif) {
-        this.codMov = codMov;
-        this.codProd = codProd;
-        this.tipo = tipo;
-        this.motivo = motivo;
-        this.referencia = referencia;
-        this.cantidad = cantidad;
-        this.codUsuario = codUsuario;
-        this.fechaCrea = fechaCrea;
-        this.fechaModif = fechaModif;
+    @Override
+    public String toString() {
+        return "movimiento_stock_DTO{"
+                + "codMov=" + codMov
+                + ", codProd=" + codProd
+                + ", tipo=" + tipo
+                + ", motivo=" + motivo
+                + ", referencia=" + referencia
+                + ", cantidad=" + cantidad
+                + ", codUsuario=" + codUsuario
+                + ", fechaCrea=" + fechaCrea
+                + ", fechaModif=" + fechaModif + '}';
     }
 
-    public int getCodMov() {
-        return codMov;
-    }
-
-    public void setCodMov(int codMov) {
-        this.codMov = codMov;
-    }
-
-    public String getCodProd() {
-        return codProd;
-    }
-
-    public void setCodProd(String codProd) {
-        this.codProd = codProd;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getMotivo() {
-        return motivo;
-    }
-
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-
-    public String getReferencia() {
-        return referencia;
-    }
-
-    public void setReferencia(String referencia) {
-        this.referencia = referencia;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public Integer getCodUsuario() {
-        return codUsuario;
-    }
-
-    public void setCodUsuario(Integer codUsuario) {
-        this.codUsuario = codUsuario;
-    }
-
-    public Timestamp getFechaCrea() {
-        return fechaCrea;
-    }
-
-    public void setFechaCrea(Timestamp fechaCrea) {
-        this.fechaCrea = fechaCrea;
-    }
-
-    public Timestamp getFechaModif() {
-        return fechaModif;
-    }
-
-    public void setFechaModif(Timestamp fechaModif) {
-        this.fechaModif = fechaModif;
-    }
 }
--- src/main/java/com/mycompany/proyectofinal/DTO/producto_DTO.java
+++ src/main/java/com/mycompany/proyectofinal/DTO/producto_DTO.java
@@ -3,73 +3,27 @@ package com.mycompany.proyectofinal.DTO;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
-public class producto_DTO {
+public record producto_DTO(
+        String codProd,
+        String descripcion,
+        String rutaImagen,
+        BigDecimal precioUnit,
+        int stockActual,
+        int codUsuario,
+        Timestamp fechaCrea,
+        Timestamp fechaModif) {
 
-    private String codProd, descripcion, rutaImagen;
-    private BigDecimal precioUnit;
-    private int stockActual;
-    private Integer codUsuario; // puede ser null
-    private Timestamp fechaCrea, fechaModif;
-
-    public producto_DTO(String codProd, String descripcion, String rutaImagen, BigDecimal precioUnit, int stockActual, Integer codUsuario, Timestamp fechaCrea, Timestamp fechaModif) {
-        this.codProd = codProd;
-        this.descripcion = descripcion;
-        this.rutaImagen = rutaImagen;
-        this.precioUnit = precioUnit;
-        this.stockActual = stockActual;
-        this.codUsuario = codUsuario;
-    }
-
-    public producto_DTO(String codProd, String descripcion, String rutaImagen, BigDecimal precioUnit, int stockActual) {
-        this.codProd = codProd;
-        this.descripcion = descripcion;
-        this.rutaImagen = rutaImagen;
-        this.precioUnit = precioUnit;
-        this.stockActual = stockActual;
-    }
-
-    public String getCodProd() {
-        return codProd;
-    }
-
-    public void setCodProd(String codProd) {
-        this.codProd = codProd;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getRutaImagen() {
-        return rutaImagen;
-    }
-
-    public void setRutaImagen(String rutaImagen) {
-        this.rutaImagen = rutaImagen;
-    }
-
-    public BigDecimal getPrecioUnit() {
-        return precioUnit;
-    }
-
-    public void setPrecioUnit(BigDecimal precioUnit) {
-        this.precioUnit = precioUnit;
-    }
-
-    public int getStockActual() {
-        return stockActual;
-    }
-
-    public void setStockActual(int stockActual) {
-        this.stockActual = stockActual;
-    }
-
-    public Integer getCodUsuario() {
-        return codUsuario;
+    @Override
+    public String toString() {
+        return "producto_DTO{"
+                + "codProd=" + codProd
+                + ", descripcion=" + descripcion
+                + ", rutaImagen=" + rutaImagen
+                + ", precioUnit=" + precioUnit
+                + ", stockActual=" + stockActual
+                + ", codUsuario=" + codUsuario
+                + ", fechaCrea=" + fechaCrea
+                + ", fechaModif=" + fechaModif + '}';
     }
 
 }
